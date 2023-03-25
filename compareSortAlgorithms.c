1f22cb77a43711059ec2c292548460bc2e3b4028
Salma Bentaj <95006614+sbentaj@users.noreply.github.com>
2023-03-25 16:54:39 -0400

    Update compareSortAlgorithms.c

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -2,12 +2,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//had to copy and paste from my VSC bc my changes werent showing up after commiting on my desktop github
+
 int extraMemoryAllocated;
+
+void merge(int pData[], int l, int r, int m)
+{
+
+	int i, j, k;
+	int n1 = m - l + 1; // creates size of one part of the array
+	int n2 = r - m;
+
+	// allocating temp memory 
+	int *L = (int*)malloc(n1*sizeof(int));
+	int *R = (int*)malloc(n2*sizeof(int));
+
+	//copying values from each side into their own arrays 
+	for (i = 0; i < n1; i++)
+	{
+		L[i] = pData[l +i]; // starts from full left side to the end of the array size
+	}
+	for(j = 0; j < n2; j++)
+	{
+		R[i] = pData[m + 1 + j]; // starts from the middle (adds 1 to go the start of 2nd arr) then goes to end of arr
+	}
+	
+	// merging them back together 
+	i = 0;
+	j = 0;
+	k = l;
+
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			pData[k] = L[i];
+			i++;
+		}
+		else 
+		{
+			pData[k] = R[j];
+			j++;
+		}
+		k++;
+
+	}
+
+	// copying over left over elements from L and R
+	while(i < n1)
+	{
+		pData[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2)
+	{
+		pData[k] = R[j];
+		j++;
+		k++;
+	}
+
+	free(L);
+	free(R);
+
+	
+}
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	if (l < r)
+	{
+		int m = (l + r)/2; // middle value of the array
+
+		//sorting both halves
+		mergeSort(pData, l, m); // keeps splitting the halves 
+		mergeSort(pData, m+1, r);
+
+		merge(pData, l, m, r); // merges them back together 
+	}
 	
 }
 
@@ -15,21 +90,63 @@ void mergeSort(int pData[], int l, int r)
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+	int i, j, item;
+
+	for (i = 1; i < n; i++)
+	{
+		item = pData[i];
+
+		for(j = i -1; j >= 0 ; j--)
+		{
+			if (pData[j] > item)
+				pData[j+1] = pData[j];
+			else
+				break;
+		}
+		pData[j+1] = item;
+
+	}
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+	int i, j, temp;
+
+	for (i = 0; i <= n-1; i++)
+	{
+		for(j = i+1; j <= n; j++)
+		{
+			if (pData[i] >= pData[j])
+			{
+				temp =  pData[j];
+				pData[j] = pData[i];
+				pData[i] = temp;
+			}
+
+		}
+	}
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+	int i, j, min_idx, temp;
+
+	for (i = 0; i < n-1; i++)
+	{
+		min_idx = i; 
+		for (j = i+1; j < n; j++)
+		{
+			if (pData[j] < pData[min_idx])
+				min_idx = j;
+		}
+		temp = pData[i];
+		pData[i] = pData[min_idx];
+		pData[min_idx] = temp;
+	}
 }
 
 // parses input file to an integer array
@@ -44,6 +161,12 @@ int parseData(char *inputFileName, int **ppData)
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)malloc(sizeof(int) * dataSz);
 		// Implement parse data block
+		
+		for(int i = 0; i < dataSz; i++)
+		{
+			fscanf(inFile, "%d", (*ppData + i));
+		}
+		fclose(inFile);
 	}
 	
 	return dataSz;
